1673353ce0e6c1e54dc1b10ce6527dadece9e2c6
Arpan Abhishek <arpanforbusiness@gmail.com>
2022-01-19 00:43:43 +0530

    complete milestone 5

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from django.urls import path
+from tasks import views
 
 urlpatterns = [
+    path("tasks/", views.view_tasks),
+    path("add-task/", views.add_task),
+    path("delete-task/<int:id>/", views.delete_task),
+    path("complete_task/<int:id>/", views.complete_task),
+    path("completed_tasks/", views.list_complete_task),
     path("admin/", admin.site.urls),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,1 +1,29 @@
-# Add your Views Here
+# Add all your views here
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from .models import Task
+
+
+def view_tasks(requests):
+		tasks = Task.objects.filter(deleted=False).filter(completed=False)
+		search_term = requests.GET.get("search_term")
+		if(search_term):
+				tasks = tasks.filter(title__icontains=search_term)
+		return render(requests, "tasks.html", {"tasks": tasks})
+
+def add_task(requests):
+		task = requests.GET.get("task")
+		Task(title=task).save()
+		return HttpResponseRedirect("/tasks")
+
+def delete_task(requests, id):
+		Task.objects.filter(id=id).update(deleted=True)
+		return HttpResponseRedirect("/tasks")
+
+def complete_task(requests, id):
+		Task.objects.filter(id=id).update(completed=True)
+		return HttpResponseRedirect("/tasks")
+
+def list_complete_task(requests):
+		completed_tasks = Task.objects.filter(deleted=False).filter(completed=True)
+		return render(requests, "completed_tasks.html", {"tasks": completed_tasks})
